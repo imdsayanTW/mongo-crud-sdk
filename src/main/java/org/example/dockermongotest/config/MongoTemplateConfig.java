65108a4c4906766b809973deb227ecc11e492a53
imdsayanTW <sayan.chowdhury@thoughtworks.com>
2025-07-23 10:31:13 +0530

    update template creations

---
+++ src/main/java/org/example/dockermongotest/config/MongoTemplateConfig.java
@@ -0,0 +1,28 @@
+package org.example.dockermongotest.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+/**
+ * Provides MongoTemplate bean for consumers.
+ */
+@Configuration
+public class MongoTemplateConfig {
+
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "testdb";
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(CONNECTION_STRING);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, DATABASE_NAME);
+    }
+}
