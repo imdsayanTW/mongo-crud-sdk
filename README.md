ed70e775ae58ea2e439df54a824379f1c4edeffc
imdsayanTW <sayan.chowdhury@thoughtworks.com>
2025-07-23 10:45:22 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test-mongo
-# test-mongo
+# This is a sample SDK that provides a Generic MongoDB CRUD operations that can be used
+# to perform basic operations on a MongoDB database from all consumer applcations.
--- src/main/java/org/example/dockermongotest/config/MongoTemplateConfig.java
+++ src/main/java/org/example/dockermongotest/config/MongoTemplateConfig.java
@@ -2,7 +2,6 @@ package org.example.dockermongotest.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
